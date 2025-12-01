e8e0ccb26d358fa539a87235cc41624f357f767d
DiegoLRC <dg.lnrd@hotmail.com>
2025-12-01 15:47:11 -0300

    [200~Fix: Correção de configuração do Prisma 6 para o Prisma 7 (Video está no Prisma 6).

--- Nest_Forum/src/user/user.controller.ts
+++ Nest_Forum/src/user/user.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Body, Post } from '@nestjs/common';
+import { Prisma, User as UserModel} from '@prisma/client';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(private readonly userService: UserService) {}
+
+
+    @Post('signup')
+    async signupUser(
+        @Body() userData: Prisma.UserCreateInput,
+    ): Promise<UserModel> {
+        return this.userService.createUser(userData);
+    }
+}
